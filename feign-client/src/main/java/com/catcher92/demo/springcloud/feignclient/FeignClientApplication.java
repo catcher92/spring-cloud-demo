0e689dc20637e615fccc151d773d7e17b908d800
catcher92 <catcher92@163.com>
2018-02-02 18:26:52 +0800

    初始化feign client

---
+++ feign-client/src/main/java/com/catcher92/demo/springcloud/feignclient/FeignClientApplication.java
@@ -0,0 +1,12 @@
+package com.catcher92.demo.springcloud.feignclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeignClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeignClientApplication.class, args);
+	}
+}
