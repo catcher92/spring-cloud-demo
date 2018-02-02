d40de5470745cfcf97a3eea22e63ef00de2d2d90
catcher92 <catcher92@163.com>
2018-02-02 16:16:01 +0800

    eureka server demo

--- eureka-server/src/main/java/com/catcher92/demo/springcloud/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/catcher92/demo/springcloud/eurekaserver/EurekaServerApplication.java
@@ -2,8 +2,10 @@ package com.catcher92.demo.springcloud.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
