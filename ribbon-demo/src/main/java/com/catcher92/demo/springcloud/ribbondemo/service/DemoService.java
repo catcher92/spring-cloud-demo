ad2844ec214864a56aa9ec7ff2baca47a82c1a42
catcher92 <catcher92@163.com>
2018-02-06 15:24:53 +0800

    ribbon使用断路器

--- ribbon-demo/src/main/java/com/catcher92/demo/springcloud/ribbondemo/service/DemoService.java
+++ ribbon-demo/src/main/java/com/catcher92/demo/springcloud/ribbondemo/service/DemoService.java
@@ -1,5 +1,6 @@
 package com.catcher92.demo.springcloud.ribbondemo.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -10,7 +11,12 @@ public class DemoService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "sayHelloFallback")
     public String sayHello(String name) {
         return restTemplate.getForObject("http://eureka-client-1/hello?name=" + name, String.class);
+    }
+
+    public String sayHelloFallback(String name) {
+        return "hi " + name + ",there is some error!";
     }
 }
