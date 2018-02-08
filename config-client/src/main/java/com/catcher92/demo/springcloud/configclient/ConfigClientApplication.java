c662b8e0bcb4a80faeea18ad7b49cc07a5bed779
catcher92 <catcher92@163.com>
2018-02-08 16:59:18 +0800

    通过bootstrap.yml中配置的cloud config server相关信息去下载配置文件，例如此处server.port就是配置在远程github中的

---
+++ config-client/src/main/java/com/catcher92/demo/springcloud/configclient/ConfigClientApplication.java
@@ -0,0 +1,24 @@
+package com.catcher92.demo.springcloud.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ConfigClientApplication {
+
+	@Value("${server.port}")
+	private String port;
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+
+	@RequestMapping("/hello")
+	public String sayHello(String name) {
+		return "Hello " + name + ", I am from port " + port;
+	}
+}
