cbdcb3d1d2047df709867d908715839423b4e3f5
catcher92 <catcher92@163.com>
2018-03-01 10:57:09 +0800

    mvc单元测试 WebEnvironment.MOCK的话server.port为-1 WebEnvironment.RANDOM_PORT的话server.port为0 WebEnvironment.DEFINED_PORT的话server.port为配置文件中server.port的值

---
+++ eureka-client/src/test/java/com/catcher92/demo/springcloud/eurekaclient/controller/IndexControllerTest.java
@@ -0,0 +1,28 @@
+package com.catcher92.demo.springcloud.eurekaclient.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class IndexControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHello() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello").param("name", "tom").accept(MediaType.APPLICATION_JSON))
+               .andExpect(content().string("Hello tom, I am from port 0"));
+    }
+}
