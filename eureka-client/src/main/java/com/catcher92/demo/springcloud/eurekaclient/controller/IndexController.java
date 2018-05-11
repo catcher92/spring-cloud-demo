15f58f6d28884b2ccb9bea70935ba22b813b1516
caoxuedong <xuedongcao2@creditease.cn>
2018-05-11 15:54:21 +0800

    spring-boot-admin改变env值后可以通过environment获取到最新值

--- eureka-client/src/main/java/com/catcher92/demo/springcloud/eurekaclient/controller/IndexController.java
+++ eureka-client/src/main/java/com/catcher92/demo/springcloud/eurekaclient/controller/IndexController.java
@@ -1,17 +1,18 @@
 package com.catcher92.demo.springcloud.eurekaclient.controller;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
 
-    @Value("${server.port}")
-    private String port;
+    @Autowired
+    private Environment environment;
 
     @GetMapping("/hello")
     public String hello(String name) {
-        return "Hello " + name + ", I am from port " + port;
+        return "Hello " + name + ", I am from port " + environment.getProperty("server.port");
     }
 }
